ad40b10223c302cdd775e0143acfaf4d05a6b171
Petr Karmashev <petr@gtnh.dev>
2026-02-02 05:11:06 +0700

    27. Remove Element

--- src/problems.rs
+++ src/problems.rs
@@ -1,1 +1,2 @@
+pub mod p27_remove_element;
 pub mod p88_merge_sorted_array;
---
+++ src/problems/p27_remove_element.rs
@@ -0,0 +1,49 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_element(nums: &mut [i32], val: i32) -> usize {
+        let (mut i, mut j) = (0, nums.len());
+
+        while i < j {
+            if nums[i] == val {
+                j -= 1;
+                if nums[j] == val {
+                    continue;
+                }
+
+                nums.swap(i, j);
+                continue;
+            }
+
+            i += 1;
+        }
+
+        i
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn remove_element() {
+        let test_cases = [
+            (vec![3, 2, 2, 3], 3, 2, vec![2, 2]),
+            (vec![0, 1, 2, 2, 3, 0, 4, 2], 2, 5, vec![0, 1, 3, 0, 4]),
+        ];
+
+        for test_case in test_cases {
+            let mut nums = test_case.0;
+            let val = test_case.1;
+            let expected_k = test_case.2;
+
+            let k = Solution::remove_element(&mut nums, val);
+
+            for i in nums.iter().take(k) {
+                assert_ne!(i, &val);
+            }
+            assert_eq!(k, expected_k);
+        }
+    }
+}
