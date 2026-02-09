113ef08386a47c41026234a0f0647ed28129e6c2
Petr Karmashev <petr@gtnh.dev>
2026-02-09 22:11:39 +0700

    134. Gas Station

--- src/problems.rs
+++ src/problems.rs
@@ -1,5 +1,6 @@
 pub mod p121_best_time_to_buy_and_sell_stock;
 pub mod p122_best_time_to_buy_and_sell_stock_ii;
+pub mod p134_gas_station;
 pub mod p13_roman_to_integer;
 pub mod p169_majority_element;
 pub mod p189_rotate_array;
---
+++ src/problems/p134_gas_station.rs
@@ -0,0 +1,46 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
+        let mut current_tank = 0;
+        let mut start = 0;
+        let mut gas_cost_total = 0;
+        for i in 0..gas.len() {
+            let current_gas_cost = gas[i] - cost[i];
+            gas_cost_total += current_gas_cost;
+            current_tank += current_gas_cost;
+            if current_tank < 0 {
+                start = i + 1;
+                current_tank = 0;
+            }
+        }
+
+        match gas_cost_total >= 0 {
+            true => start as i32,
+            false => -1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let test_cases = [
+            (vec![1, 2, 3, 4, 5], vec![3, 4, 5, 1, 2], 3),
+            (vec![2, 3, 4], vec![3, 4, 3], -1),
+        ];
+
+        for test_case in test_cases {
+            let gas = test_case.0;
+            let cost = test_case.1;
+            let expected = test_case.2;
+
+            let result = Solution::can_complete_circuit(gas, cost);
+
+            assert_eq!(expected, result);
+        }
+    }
+}
